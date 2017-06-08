62388e98f879cbd6ff38ff8e229db09ee6c434bc
vipul0092 <sunny3091@gmail.com>
2017-06-09 00:33:20 +0530

    Adding support for less files processing

--- sivalk-app/webpack.config.js
+++ sivalk-app/webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -46,6 +47,16 @@ module.exports = {
                 test: /\.ico$/,
                 loader: 'file-loader?name=[name].[ext]'
             },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            }
         ]
     },
     stats: {
